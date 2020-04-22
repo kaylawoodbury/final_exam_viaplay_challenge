de616333b7ec46725d9a9ed7cea9651b39c60472
kaylawoodbury <kaylasandoval92@gmail.com>
2020-04-22 17:23:13 +0200

    not completed, but was able to access the content using debuggers in the inspect window, updated app jsx to try to return the image

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import React, { useEffect } from 'react'
 import { fetchVideos } from './modules/fetchVideos'
 
 const App = () => {
+  let series
   // useEffect(() => {
     fetchVideos().then((data) => {
       debugger
-      let series = data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']
+      series = data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']
     })
-  // }, [])
+  //  }, [])
 
-  let image
-   let seriesList = series.map(show => {
-     image = show.content.images.coverart23.template
+  let seriesList
+  if(series){
+   seriesList = series.map(show => {
+     return (
+     <div>
+       <img
+       src={show.content.images.landscape.url}
+       alt={show.content.title}
+       />
+     </div>
+     )
   })
+}
 
   return (
     <div>
